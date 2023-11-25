2ea6d0f6f0a8728a4b5cbd45669ccb875dc90818
ghinaaa0 <108055830+ghinaaa0@users.noreply.github.com>
2023-11-25 16:18:17 +0700

    siswas.js

--- router/siswas.js
+++ router/siswas.js
@@ -6,10 +6,10 @@ router.get('/siswas', siswacontroller.index)
 
 router.get('/siswa/:id', siswacontroller.show) 
 
-  router.post('/siswa', siswacontroller.store)
+  router.post('/siswa/add', siswacontroller.store)
   
-  router.put('/siswa/:id', siswacontroller.update)
+  router.put('/siswa/update/:id', siswacontroller.update)
 
-  router.delete('/siswa/:id', siswacontroller.delete)
+  router.delete('/siswa/delete/:id', siswacontroller.delete)
 
   module.exports = router
